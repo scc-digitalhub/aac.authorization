0d886901c78a3446617449e458679ab55f618597
mirkoperillo <mirko.perillo@gmail.com>
2017-05-15 11:51:38 +0200

    rollback default constructor

--- src/main/java/it/smartcommunitylab/aac/authorization/model/AuthorizationNode.java
+++ src/main/java/it/smartcommunitylab/aac/authorization/model/AuthorizationNode.java
@@ -28,8 +28,6 @@ public class AuthorizationNode {
 		this.qname = qname;
 	}
 
-	public AuthorizationNode() {
-	}
 
 	@PersistenceConstructor
 	private AuthorizationNode(String qname, final List<AuthorizationNodeParam> parameters) {
