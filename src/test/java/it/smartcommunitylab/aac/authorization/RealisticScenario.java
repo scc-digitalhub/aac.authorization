e001e4cee8c8bec6420fd73ab03afb1e3d82756b
mirkoperillo <mirko.perillo@gmail.com>
2017-05-24 11:55:53 +0200

    added test scenarios

--- src/test/java/it/smartcommunitylab/aac/authorization/RealisticScenario.java
+++ src/test/java/it/smartcommunitylab/aac/authorization/RealisticScenario.java
@@ -50,11 +50,11 @@ public class RealisticScenario {
 	public void setup() {
 		try {
 			AuthorizationNode student = new AuthorizationNode(new FQname(DOMAIN, "student"));
-			student.addParameter("student");
+			student.addParameter("studentId");
 			AuthorizationNode institute = new AuthorizationNode(new FQname(DOMAIN, "institute"));
-			institute.addParameter("institute");
+			institute.addParameter("instituteId");
 			AuthorizationNode certifier = new AuthorizationNode(new FQname(DOMAIN, "certifier"));
-			institute.addParameter("certifier");
+			institute.addParameter("certifierId");
 
 			schemaHelper.addRootChild(student);
 			schemaHelper.addRootChild(institute);
@@ -63,6 +63,12 @@ public class RealisticScenario {
 			AuthorizationNode data = new AuthorizationNode(new FQname(DOMAIN, "data"));
 			data.addParameter("data");
 			schemaHelper.addChild(student.getFqname(), data);
+
+			AuthorizationNode dataDetail = new AuthorizationNode(new FQname(DOMAIN, "data-detail"));
+			dataDetail.addParameter("type");
+			dataDetail.addParameter("id");
+
+			schemaHelper.addChild(data.getFqname(), dataDetail);
 
 			AuthorizationNode dataInstitute = new AuthorizationNode(new FQname(DOMAIN, "data-institute"));
 			dataInstitute.addParameter("data");
@@ -91,13 +97,13 @@ public class RealisticScenario {
 	public void scenarioFirstLevelAuthorization() throws NotValidResourceException {
 		AuthorizationUser subject = new AuthorizationUser("my-id", "user");
 		AuthorizationUser entity = new AuthorizationUser("my-id", "user");
-		Resource resStudent1 = new Resource(new FQname(DOMAIN, "student"),
-				Arrays.asList(new AuthorizationNodeValue("student", "student", "my-id")));
+		Resource resStudent1 = new Resource(new FQname(DOMAIN, "student"));
+		resStudent1.addNodeValue(new AuthorizationNodeValue("student", "studentId", "my-id"));
 		Authorization authorization = new Authorization(subject, "action", resStudent1, entity);
 		authHelper.insert(authorization);
 
-		Resource resStudent2 = new Resource(new FQname(DOMAIN, "student"),
-				Arrays.asList(new AuthorizationNodeValue("student", "student", "he-man-id")));
+		Resource resStudent2 = new Resource(new FQname(DOMAIN, "student"));
+		resStudent2.addNodeValue(new AuthorizationNodeValue("student", "studentId", "he-man-id"));
 		Authorization authorization1 = new Authorization(subject, "action", resStudent2, entity);
 
 		Assert.assertFalse(authHelper.validate(authorization1));
@@ -106,16 +112,16 @@ public class RealisticScenario {
 
 		// try validate a second level authorization
 
-		Resource resStudent3 = new Resource(new FQname(DOMAIN, "data"),
-				Arrays.asList(new AuthorizationNodeValue("student", "student", "my-id"),
-						new AuthorizationNodeValue("data", "data", "Experience")));
+		Resource resStudent3 = new Resource(new FQname(DOMAIN, "data"));
+		resStudent3.addNodeValue(new AuthorizationNodeValue("student", "studentId", "my-id"));
+		resStudent3.addNodeValue(new AuthorizationNodeValue("data", "data", "Experience"));
 		Authorization authorization2 = new Authorization(subject, "action", resStudent3, entity);
 
 		Assert.assertTrue(authHelper.validate(authorization2));
 
-		Resource resStudent4 = new Resource(new FQname(DOMAIN, "data"),
-				Arrays.asList(new AuthorizationNodeValue("student", "student", "he-man-id"),
-						new AuthorizationNodeValue("data", "data", "Experience")));
+		Resource resStudent4 = new Resource(new FQname(DOMAIN, "data"));
+		resStudent4.addNodeValue(new AuthorizationNodeValue("student", "studentId", "he-man-id"));
+		resStudent4.addNodeValue(new AuthorizationNodeValue("data", "data", "Experience"));
 		Authorization authorization3 = new Authorization(subject, "action", resStudent4, entity);
 
 		Assert.assertFalse(authHelper.validate(authorization3));
@@ -125,14 +131,14 @@ public class RealisticScenario {
 	public void scenarioSecondLevelAuthorization() throws NotValidResourceException {
 		AuthorizationUser subject = new AuthorizationUser("my-id", "user");
 		AuthorizationUser entity = new AuthorizationUser("my-id", "user");
-		Resource resStudent1 = new Resource(new FQname(DOMAIN, "data"),
-				Arrays.asList(new AuthorizationNodeValue("data", "data", "Experience"),
-						new AuthorizationNodeValue("student", "student", "my-id")));
+		Resource resStudent1 = new Resource(new FQname(DOMAIN, "data"));
+		resStudent1.addNodeValue(new AuthorizationNodeValue("data", "data", "Experience"));
+		resStudent1.addNodeValue(new AuthorizationNodeValue("student", "studentId", "my-id"));
 		Authorization authorization = new Authorization(subject, "action", resStudent1, entity);
 		authHelper.insert(authorization);
 
-		Resource resStudent2 = new Resource(new FQname(DOMAIN, "student"),
-				Arrays.asList(new AuthorizationNodeValue("student", "student", "he-man")));
+		Resource resStudent2 = new Resource(new FQname(DOMAIN, "student"));
+		resStudent2.addNodeValue(new AuthorizationNodeValue("student", "studentId", "he-man"));
 		Authorization authorization1 = new Authorization(subject, "action", resStudent2, entity);
 
 		Assert.assertFalse(authHelper.validate(authorization1));
@@ -140,22 +146,68 @@ public class RealisticScenario {
 
 	}
 
+	@Test
+	public void scenarioThirdLevelAuthorization() throws NotValidResourceException {
+		AuthorizationUser subject = new AuthorizationUser("my-id", "user");
+		AuthorizationUser entity = new AuthorizationUser("my-id", "user");
+		Resource resStudent1 = new Resource(new FQname(DOMAIN, "data"));
+		resStudent1.addNodeValue(new AuthorizationNodeValue("data", "data", "Experience"));
+		resStudent1.addNodeValue(new AuthorizationNodeValue("student", "studentId", "my-id"));
+		Authorization authorization = new Authorization(subject, "action", resStudent1, entity);
+		authHelper.insert(authorization);
+
+		AuthorizationUser subject1 = new AuthorizationUser("my-id", "user");
+		AuthorizationUser entity1 = new AuthorizationUser("my-id", "user");
+		Resource resStudent2 = new Resource(new FQname(DOMAIN, "data-detail"));
+		resStudent2.addNodeValue(new AuthorizationNodeValue("data", "data", "Experience"));
+		resStudent2.addNodeValue(new AuthorizationNodeValue("student", "studentId", "my-id"));
+		resStudent2.addNodeValue(new AuthorizationNodeValue("data-detail", "type", "expType"));
+		resStudent2.addNodeValue(new AuthorizationNodeValue("data-detail", "id", "expId"));
+		Authorization authorization1 = new Authorization(subject1, "action", resStudent2, entity1);
+		Assert.assertTrue(authHelper.validate(authorization1));
+
+		Resource resStudent3 = new Resource(new FQname(DOMAIN, "data"));
+		resStudent3.addNodeValue(new AuthorizationNodeValue("data", "data", "Registration"));
+		resStudent3.addNodeValue(new AuthorizationNodeValue("student", "studentId", "my-id"));
+		Authorization authorization2 = new Authorization(subject, "action", resStudent3, entity);
+		Assert.assertFalse(authHelper.validate(authorization2));
+
+		Resource resStudent4 = new Resource(new FQname(DOMAIN, "data"));
+		resStudent3.addNodeValue(new AuthorizationNodeValue("student", "studentId", "my-id"));
+		Authorization authorization3 = new Authorization(subject, "action", resStudent4, entity);
+		Assert.assertFalse(authHelper.validate(authorization3));
+	}
+
 
 	@Test
 	public void incompleteResource() throws NotValidResourceException {
 		AuthorizationUser subject = new AuthorizationUser("my-id", "user");
 		AuthorizationUser entity = new AuthorizationUser("my-id", "user");
-		Resource resStudent1 = new Resource(new FQname(DOMAIN, "student"),
-				Arrays.asList(new AuthorizationNodeValue("student", "student", "my-id")));
+		Resource resStudent1 = new Resource(new FQname(DOMAIN, "student"));
+		resStudent1.addNodeValue(new AuthorizationNodeValue("student", "studentId", "my-id"));
 		Authorization authorization = new Authorization(subject, "action", resStudent1, entity);
 		authHelper.insert(authorization);
 
-		Resource resStudent3 = new Resource(new FQname(DOMAIN, "data"),
-				Arrays.asList(new AuthorizationNodeValue("data", "data", "Experience")));
+		Resource resStudent3 = new Resource(new FQname(DOMAIN, "data"));
+		resStudent3.addNodeValue(new AuthorizationNodeValue("data", "data", "Experience"));
 		Authorization authorization2 = new Authorization(subject, "action", resStudent3, entity);
 
 		Assert.assertFalse(authHelper.validate(authorization2));
+	}
 
+	@Test
+	public void authorizeAllValue() throws NotValidResourceException {
+		AuthorizationUser subject = new AuthorizationUser("my-id", "user");
+		AuthorizationUser entity = new AuthorizationUser("my-id", "user");
+		Resource resStudent1 = new Resource(new FQname(DOMAIN, "student"),
+				Arrays.asList(new AuthorizationNodeValue("student", "studentId", AuthorizationNodeValue.ALL_VALUE)));
+		Authorization authorization = new Authorization(subject, "action", resStudent1, entity);
+		authHelper.insert(authorization);
+
+		Resource resStudent2 = new Resource(new FQname(DOMAIN, "student"),
+				Arrays.asList(new AuthorizationNodeValue("student", "studentId", "eddie-brock")));
+		Authorization authorization1 = new Authorization(subject, "action", resStudent2, entity);
+		Assert.assertTrue(authHelper.validate(authorization1));
 	}
 }
 
